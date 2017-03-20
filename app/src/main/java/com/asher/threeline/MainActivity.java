23f52c1760fbbbda7fccc28c3f606ea07d251f92
ouyangfan <ouyangfan1991@gmail.com>
2017-03-20 20:42:11 +0800

    增加 butterKnife与 dagger2 支持

--- app/src/main/java/com/asher/threeline/MainActivity.java
+++ app/src/main/java/com/asher/threeline/MainActivity.java
@@ -2,12 +2,34 @@ package com.asher.threeline;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.tv_show)
+    TextView tvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_show)
+    void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_show:
+                Toast.makeText(this, "Hello Asher", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
