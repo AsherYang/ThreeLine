fa2fb8610c244905c45103d0519e4ace72844738
ouyangfan <ouyangfan1991@gmail.com>
2017-04-22 23:48:09 +0800

    1.  换肤注解

---
+++ app/src/main/java/com/asher/threeline/aop/annotation/Skin.java
@@ -0,0 +1,16 @@
+package com.asher.threeline.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by ouyangfan on 17/4/22.
+ *
+ * 换肤注解
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
+public @interface Skin {
+}
