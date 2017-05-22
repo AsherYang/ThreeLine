ad87009b9f10b5becdd60338c3cd4effe2003031
ouyangfan <ouyangfan1991@gmail.com>
2017-05-23 00:41:05 +0800

    add search job for me

---
+++ server/job/JobZhilian.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+# - * - coding:utf-8 -*-
+
+
+"""
+Author: AsherYang
+Email:  1181830457@qq.com
+Date:   2017/5/23
+Desc:   智联招聘类
+"""
+
+ZHILIAN_LOGIN_URL = ''
+
+class ZhilianJob():
+
+    def login(self):
+        return
+
