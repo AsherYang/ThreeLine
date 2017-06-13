13a50908fcecef8407c7a9e9d108b9e0af286b42
ouyangfan <ouyangfan1991@gmail.com>
2017-06-13 09:41:55 +0800

    complete job zhilian auto refresh

--- server/job/JobZhilian.py
+++ server/job/JobZhilian.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python
 # - * - coding:utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.support.ui import  WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 """
 Author: AsherYang
@@ -33,13 +34,25 @@ class ZhilianJob():
         passwordFieldElement = WebDriverWait(driver, 10).until(lambda driver : driver.find_elements_by_css_selector(passwordFieldCssSelect))
         loginBtnFieldElement = WebDriverWait(driver, 10).until(lambda driver : driver.find_elements_by_css_selector(loginBtnFieldCssSelect))
 
-        userNameFieldElement.clear()
-        userNameFieldElement.send_keys(self.name)
-        passwordFieldElement.clear()
-        passwordFieldElement.send_keys(self.pwd)
-        loginBtnFieldElement.click()
+        # print type(userNameFieldElement)
+        # userNameFieldElement passwordFieldCssSelect is a list in zhilian(tab)
+        userNameFieldElement[0].clear()
+        userNameFieldElement[0].send_keys(self.name)
+        passwordFieldElement[0].clear()
+        passwordFieldElement[0].send_keys(self.pwd)
+        loginBtnFieldElement[0].click()
+
+        logoCssSelect = 'div.nav_img.fl > img'
+        WebDriverWait(driver, 10).until(lambda driver : driver.find_elements_by_css_selector(logoCssSelect))
+        refreshCssSelect = 'div.zhImgCole > div.myLink > a.myLinkA.linkRefresh > i'
+        refreshFieldElement = WebDriverWait(driver, 10).until(lambda driver : driver.find_elements_by_css_selector(refreshCssSelect))
+        refreshFieldElement[0].click()
+
+        time.sleep(5)
+        driver.close()
+        driver.quit()
 
 if __name__ == "__main__":
     zhilian = ZhilianJob()
-    zhilian.setLoginInfo('1181830457@qq.com', '1991429ouyangfan')
+    zhilian.setLoginInfo('oyf1991@126.com', '1991429ouyangfan')
     zhilian.login()
