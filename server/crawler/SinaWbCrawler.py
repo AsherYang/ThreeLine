37343b8bf698fb91aa027a063f4843863914e5b3
ouyangfan <ouyangfan1991@gmail.com>
2017-08-07 23:26:09 +0800

    add sina weibo crawler

---
+++ server/crawler/SinaWbCrawler.py
@@ -0,0 +1,17 @@
+"""
+Author: AsherYang
+Email:  ouyangfan1991@gmail.com
+Date:   17/8/7
+Desc:   sina weibo crawler
+"""
+
+class SinaWbCrawler() :
+    def __init__(self):
+        print 'sina weibo crawler.'
+
+    def do(self):
+        return '--- sina wei bo msg.'
+
+if __name__ == '__main__':
+    sinaWbCrawler = SinaWbCrawler()
+    print sinaWbCrawler.do()
