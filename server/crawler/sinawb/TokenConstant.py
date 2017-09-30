a1e451ab3525c5a0852782d1990f848b2329cb72
ouyangfan <ouyangfan1991@gmail.com>
2017-09-30 17:30:32 +0800

    add sinawb token

---
+++ server/crawler/sinawb/TokenConstant.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Author: AsherYang
+Email:  ouyangfan1991@gmail.com
+Date:   2017/9/22.
+Desc:  sinaWeibo appkey
+@see:  http://open.weibo.com/apps/2489615368/info/basic?action=review
+"""
+
+domain="https://api.weibo.com/2/"
+token=""
+appkey = "2489615368"
+secret = "dbb84df92e9a9c8f8e10d9985a8038a8"
