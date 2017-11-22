24b5fce4281aaee75c1f2763bf4b8b8c5f265da2
AsherYang <ouyangfan1991@gmail.com>
2017-11-22 09:44:36 +0800

    sina weibo crawler access token

--- server/crawler/sinawb/SinaWbCrawler.py
+++ server/crawler/sinawb/SinaWbCrawler.py
@@ -1,21 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 Author: AsherYang
-Email:  ouyangfan1991@gmail.com
-Date:   17/8/7
-Desc:   sina weibo crawler
+Email : ouyangfan1991@gmail.com
+Date  : 2017/11/22
+Desc  : sina weibo crawler
+@see  :  http://open.weibo.com/tools/console
+使用文档中"授权机制" -> "开发者自身的授权" -> "接口测试工具"(Token 也是此自身授权)
+需要区分OAuth2.0授权： 是指应用需要用户授权用户的新浪微博权限给应用，可用来读取授权用户的微博信息。
+此处，我们只是用自己的授权，即开发者自身的授权，用于读取自身的微博数据。
 """
 
-BASE_URL = 'https://api.weibo.com/2/'
-NEW_PUSH_URL = BASE_URL + 'statuses/user_timeline.json'
+from TokenConstant import *
+
+BASE_URL = sina_domain
+# NEW_PUSH_URL = BASE_URL + 'statuses/user_timeline.json'
+# TOKEN_URL = BASE_URL + 'OAuth2/authorize'
+
 class SinaWbCrawler() :
     def __init__(self):
         print 'sina weibo crawler.'
 
     def do(self):
-        return '--- sina wei bo msg.'
+        return self.getAccessToken()
 
     def getAccessToken(self):
-        return "1234"
+        return sina_token
 
 if __name__ == '__main__':
     sinaWbCrawler = SinaWbCrawler()
--- server/crawler/sinawb/TokenConstant.py
+++ server/crawler/sinawb/TokenConstant.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 """
 Author: AsherYang
 Email:  ouyangfan1991@gmail.com
 Date:   2017/9/22.
 Desc:  sinaWeibo appkey
 @see:  http://open.weibo.com/apps/2489615368/info/basic?action=review
+use sina author 52*****18@qq.com
 """
 
-domain="https://api.weibo.com/2/"
-token=""
-appkey = "2489615368"
-secret = "dbb84df92e9a9c8f8e10d9985a8038a8"
+sina_domain="https://api.weibo.com/2/"
+sina_token="2.008qwMJEagKUiCa7196fa2370UKTrM"
+sina_appkey = "2489615368"
+sina_secret = "dbb84df92e9a9c8f8e10d9985a8038a8"
