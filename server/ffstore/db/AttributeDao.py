28d722395af151178112282e51031633847fc99d
AsherYang <ouyangfan1991@gmail.com>
2018-05-24 09:13:13 +0800

    mysql WHERE IN sentence

--- server/ffstore/db/AttributeDao.py
+++ server/ffstore/db/AttributeDao.py
@@ -10,6 +10,7 @@ Desc  : 操作属性数据表
 """
 from ffstore.util import DbUtil
 
+
 class AttributeDao:
     def __init__(self):
         pass
@@ -17,6 +18,8 @@ class AttributeDao:
     # 根据给定的分类列表获取各个分类的属性描述
     # 返回包含查询分类的属性值集合(多个分类，每个分类可能有多条属性)
     def queryCateAttrs(self, cateIdList):
-        # todo
-        query = 'SELECT * FROM ffstore_attr WHERE cate_id IN (%s)' % cateIdList
+        if cateIdList is None:
+            return None
+        format_str = ','.join(['%s'] * len(cateIdList))
+        query = 'SELECT * FROM ffstore_attr WHERE cate_id IN (%s)' % format_str, tuple(cateIdList)
         return DbUtil.query(query)
