26ec518eceda9ea574a4b35613323f70dcad0722
ouyangfan <ouyangfan1991@gmail.com>
2018-05-26 08:23:19 +0800

    category show type

---
+++ server/ffstore/constant/CategoryShowType.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+Author: AsherYang
+Email:  ouyangfan1991@gmail.com
+Date:   2018/5/22
+Desc:   分类显示类型常量定义
+
+对应 ffstore_category 表 cate_show_type 字段
+"""
+
+# 一般显示类型
+TYPE_SHOW_NORMAL = 0
+# 首页展示类型
+TYPE_SHOW_HOME = 1
