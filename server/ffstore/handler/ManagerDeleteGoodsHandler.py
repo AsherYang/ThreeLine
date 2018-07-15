7a8358dab54846a536464486f5a70345147905c6
ouyangfan <ouyangfan1991@gmail.com>
2018-07-15 23:03:47 +0800

    add missed commit file

---
+++ server/ffstore/handler/ManagerDeleteGoodsHandler.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author: AsherYang
+Email : ouyangfan1991@gmail.com
+Date  : 2018/7/5
+Desc  : 后台管理系统， 删除商品handler
+
+manager api handler
+
+"""
+
+import sys
+sys.path.append('../')
+import tornado.web
+from constant import ResponseCode
+from FFStoreJsonEncoder import *
+
+from mgrsys.PermissionManager import PermissionManager
+from net.GetGoods import GetGoods
+
+
+class ManagerDeleteGoodsHandler(tornado.web.RequestHandler):
+    def post(self, *args, **kwargs):
+        param = self.request.body.decode('utf-8')
+        prarm = json.loads(param)
+        sign = prarm['sign']
+        time = prarm['time']
+        admin_tel = prarm['tel']
+        sms_pwd = prarm['sms']
+        goods_id = param['goodsid']
+
+        permissionMgr = PermissionManager()
+        baseResponse = permissionMgr.checkAdminPermissionWithLoginStatus(sign=sign, time=time,
+                                                                         admin_tel=admin_tel, sms_pwd=sms_pwd)
+        if baseResponse.code == ResponseCode.success_check_admin_permission:
+            getGoods = GetGoods()
+            deleteResult = getGoods.deleteByGoodsId(goods_id=goods_id)
+            if deleteResult:
+                # todo 删除商品对应保存的图片
+                baseResponse.code = ResponseCode.op_success
+                baseResponse.desc = ResponseCode.op_success_desc
+            else:
+                baseResponse.code = ResponseCode.fail_op_db_data
+                baseResponse.desc = ResponseCode.fail_op_db_data_desc
+        json_str = json.dumps(baseResponse, cls=StrEncoder)
+        self.write(json_str)
+
+
---
+++ server/ffstore/handler/ManagerUpdateGoodsHandler.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author: AsherYang
+Email : ouyangfan1991@gmail.com
+Date  : 2018/7/5
+Desc  : 后台管理系统， 添加商品handler
+
+manager api handler
+
+"""
+
+import sys
+sys.path.append('../')
+import tornado.web
+from constant import ResponseCode
+from FFStoreJsonEncoder import *
+
+from mgrsys.PermissionManager import PermissionManager
+from net.GetGoods import GetGoods
+
+
+class ManagerUpdateGoodsHandler(tornado.web.RequestHandler):
+    def post(self, *args, **kwargs):
+        param = self.request.body.decode('utf-8')
+        prarm = json.loads(param)
+        sign = prarm['sign']
+        time = prarm['time']
+        admin_tel = prarm['tel']
+        sms_pwd = prarm['sms']
+        goods_id = param['goodsid']
+        cate_id = param['cateid']
+        brand_id = param['brandid']
+        goods_name = param['name']
+        market_price = param['marketprice']
+        current_price = param['currentprice']
+        sale_count = param['salecount']
+        stock_num = param['stocknum']
+        goods_code = param['goodscode']
+        goods_logo = param['goodslogo']
+        thum_logo = param['thumlogo']
+        keywords = param['keywords']
+
+        permissionMgr = PermissionManager()
+        baseResponse = permissionMgr.checkAdminPermissionWithLoginStatus(sign=sign, time=time,
+                                                                         admin_tel=admin_tel, sms_pwd=sms_pwd)
+        if baseResponse.code == ResponseCode.success_check_admin_permission:
+            getGoods = GetGoods()
+            dbGoods = getGoods.getGoodsById(goods_id=goods_id)
+            if not dbGoods:
+                baseResponse.code = ResponseCode.fail_goods_not_found
+                baseResponse.desc = ResponseCode.fail_goods_not_found_desc
+            else:
+                # 更新数据
+                if cate_id:
+                    dbGoods.cate_id = cate_id
+                if brand_id:
+                    dbGoods.brand_id = brand_id
+                if goods_name:
+                    dbGoods.goods_name = goods_name
+                if market_price:
+                    dbGoods.market_price = market_price
+                if current_price:
+                    dbGoods.current_price = current_price
+                if sale_count:
+                    dbGoods.sale_count = sale_count
+                if stock_num:
+                    dbGoods.stock_num = stock_num
+                if goods_code:
+                    dbGoods.goods_code = goods_code
+                if goods_logo:
+                    dbGoods.goods_logo = goods_logo
+                if thum_logo:
+                    dbGoods.thum_logo = thum_logo
+                if keywords:
+                    dbGoods.keywords = keywords
+                updateResult = getGoods.updateToDb(goods=dbGoods)
+                if updateResult:
+                    baseResponse.code = ResponseCode.op_success
+                    baseResponse.desc = ResponseCode.op_success_desc
+                else:
+                    baseResponse.code = ResponseCode.fail_op_db_data
+                    baseResponse.desc = ResponseCode.fail_op_db_data_desc
+        json_str = json.dumps(baseResponse, cls=StrEncoder)
+        self.write(json_str)
+
+
